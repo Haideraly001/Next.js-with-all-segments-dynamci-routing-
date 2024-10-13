407acfc427435a4bdef6fe8f9f8d73706f1ff43d
Haider Aly <haideralymughal@gmail.com>
2024-10-13 08:43:20 +0500

    use the image in tag

---
+++ src/app/ImgTag/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Images = () => {
+  return (
+    <div>
+      <h2>image practice</h2>
+      <img src="https://images.unsplash.com/photo-1472214103451-9374bd1c798e?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" width={900} height={500} />
+    </div>
+  )
+}
+
+export default Images
